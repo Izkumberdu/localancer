c91e582335c96f849e1c8e1a8487aef4b723e7d0
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2023-11-22 11:07:57 +0800

    added app styles and size config

---
+++ lib/app_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Colors used at login and register
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kLightGrey = Color(0xFFF3F4F8);
+const Color kPink = Color(0xFFFA0078);
+const Color kMediumGrey = Color(0xFF808080);
+const Color kDarkBlue = Color(0xFF111828);
